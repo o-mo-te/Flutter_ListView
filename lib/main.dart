3c7f70d1e12ab6e7f20e45c15f3e0d96ce4eb67b
hidetomo117 <hidetomo117@gmail.com>
2020-05-26 14:55:57 +0900

    add save function

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
 
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -28,6 +29,9 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+          ],
       ),
       body: _buildSuggestions(),
     );
@@ -58,10 +62,42 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
-        style: _biggerFont,)
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+        if (alreadySaved) {
+          _saved.remove(pair);
+        } else {
+          _saved.add(pair);
+        }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
+            return ListTile(title: Text(pair.asPascalCase, style: _biggerFont,),);
+        });
+        final List<Widget> devided = ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved Suggestions'),
+          ),
+          body: ListView(children: devided),
+        );
+      }),
     );
   }
 
